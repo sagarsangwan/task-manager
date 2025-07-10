45fcaa46679ac7b9c37af5cf8a6731aa14cbe92b
sagar sangwan <iamsagarsangwan@gmail.com>
2025-07-10 13:45:52 +0530

    added view for task

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serialize_class = TaskSerializer
